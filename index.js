80ec5785994d26a74aae7d94d96f2524bdacb874
hjpark8907 <msyamun@gmail.com>
2021-04-29 02:15:04 +0900

    heroku 배포 준비

--- index.js
+++ index.js
@@ -1,28 +1,143 @@
-var http = require('http');
-var hostname = '127.0.0.1'; // point the local computer
-var port = 8080;
-
-const server = http.createServer(function (req, res) {
-    const path = req.url;
-    const method = req.method;
-    if (path === '/products') {
-        if (method === 'GET') {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            const products = JSON.stringify([
-                {
-                    name: "basket ball",
-                    price: 5000
-                }
-            ])
-            res.end(products);
-        } else if (method === 'POST') {
-            res.end("It is made!");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const models = require('./models');
+const multer = require('multer');
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads/')
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
         }
-    }
-    res.end("Good Bye");
+    })
+});
 
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.get('/banners', (req, res) => { // 배너를 주는 API
+    models.Banner.findAll({
+        limit: 3
+    }).then((result) => {
+        res.send({
+            banners: result,
+        });
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).send('error happens!');
+    })
+})
+app.get("/products", (req, res) => {
+    models.Product.findAll(
+        {
+            limit: 10,
+            order: [
+                ['createdAt', 'DESC'],
+            ],
+            attributes: [
+                'id',
+                'name',
+                'price',
+                'createdAt',
+                'seller',
+                'imageUrl',
+                'soldout',
+            ] // 보여줄것만 선택적으로 보여줌
+        }
+    ).then((result) => {
+        console.log("PRODUCTS : ", result);
+        res.send({
+            products: result
+        })
+    }).catch((error) => {
+        console.error(error);
+        res.status(400).send("errer happens!");
+    })
+});
+
+app.post("/products", (req, res) => {
+    const body = req.body;
+    const { name, description, price, seller, imageUrl } = body;
+    if (!name || !description || !price || !seller || !imageUrl) {
+        res.status(400).send("All field should be write!!")
+    }
+    models.Product.create({
+        name,
+        description,
+        price,
+        seller,
+        imageUrl,
+    }).then((result) => {
+        console.log("Product Creation result : ", result);
+        res.send({
+            result,
+        });
+    }).catch((error) => {
+        console.error(error);
+        res.status(400).send("Problems happen when products is uploading!")
+    })
 })
 
-server.listen(port, hostname);
+app.get("/products/:id", (req, res) => {
+    const params = req.params;
+    const { id, eventid } = params;
+    models.Product.findOne({
+        where: {
+            id: id
+        }
+    }).then((result) => {
+        console.log("PRODUCT : ", result);
+        res.send({
+            product: result,
+        });
+    }).catch((error) => {
+        console.error(error);
+        res.status(400).send("find products error!");
+    })
+})
 
-console.log('hj market server on!');
+app.post('/image', upload.single('image'), (req, res) => {
+    const file = req.file;
+    console.log(file);
+    res.send({
+        imageUrl: file.path,
+    })
+})
+
+app.post("/purchase/:id", (req, res) => {
+    const { id } = req.params;
+    models.Product.update(
+        {
+            soldout: 1
+        },
+        {
+            where: {
+                id
+            }
+        }
+    ).then((result) => {
+        res.send({
+            result: true
+        })
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).send("error happens!");
+    })
+});
+
+
+app.listen(port, () => {
+    console.log("Hj showpping mall server is acting.");
+    models.sequelize.sync().then(() => {
+        console.log('DB connection success!');
+    }).catch((error) => {
+        console.error(error);
+        console.log("DB connection error!");
+        process.exit();
+    })
+})
